1437ff9c043457945f21c640c17a7f9e97d59682
Douglas Young <dyoung4747@hotmail.com>
2021-08-16 19:44:22 +0000

    refactoring with input file

--- client.js
+++ client.js
@@ -1,23 +1,5 @@
 const net = require('net');
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  handleUserInput();
-  return stdin;
-}
-
-const handleUserInput = function() {
-  process.stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-    process.stdout.write(key)
-  })
-}
-
 const connect = function() {
   const conn = net.createConnection({
     host: 'localhost',
