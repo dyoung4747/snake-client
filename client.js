fa695ee2ff54eed712574013b600f05aca28666b
Douglas Young <dyoung4747@hotmail.com>
2021-08-16 19:35:53 +0000

    stdin setup

--- client.js
+++ client.js
@@ -1,5 +1,23 @@
 const net = require('net');
 
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+  return stdin;
+}
+
+const handleUserInput = function() {
+  process.stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+    process.stdout.write(key)
+  })
+}
+
 const connect = function() {
   const conn = net.createConnection({
     host: 'localhost',
@@ -12,6 +30,8 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
   });
+
+  setupInput();
   // conn.on('connect', () => {
   //   setInterval(() => {
   //     conn.write(('Move: up'));
@@ -23,5 +43,7 @@ const connect = function() {
 
   return conn;
 }
+
+
 
 module.exports = { connect };
