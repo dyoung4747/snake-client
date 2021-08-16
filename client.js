e254e54bdf03537c1762b64cef724791dafc35f0
Douglas Young <dyoung4747@hotmail.com>
2021-08-16 18:59:38 +0000

    adding event handlers

--- client.js
+++ client.js
@@ -5,7 +5,13 @@ const connect = function() {
     host: 'localhost',
     port: 50541
   });
+  conn.on('connect', () => {
+    conn.write('Name: DNY');
+  });
   conn.setEncoding('utf8');
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
   conn.on('data', (data) => {
     console.log('Server says: ', data);
   });
