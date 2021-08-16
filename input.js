e70d26d5bff1c081b755a8eae5181fdf71fa69eb
Douglas Young <dyoung4747@hotmail.com>
2021-08-16 22:39:33 +0000

    sending messages

--- input.js
+++ input.js
@@ -4,17 +4,25 @@ const handleUserInput = function(key) {
     if (key === '\u0003') {
       process.exit();
     } else if (key === 'w') {
-      connection.write('Move: up')
+      connection.write('Move: up');
       console.log('up');
     } else if (key === 'a') {
-      connection.write('Move: left')
+      connection.write('Move: left');
       console.log('left');
     } else if (key === 's') {
-      connection.write('Move: down')
+      connection.write('Move: down');
       console.log('down');
     } else if (key === 'd') {
-      connection.write('Move: right')
+      connection.write('Move: right');
       console.log('right');
+    } else if (key === '`') {
+      connection.write('Say: You won\'t beat me!');
+    } else if (key === '1') {
+      connection.write('Say: You\'re going downnnnn...');
+    } else if (key === '2') {
+      connection.write('Say: You got lucky...');
+    } else if (key === '3') {
+      connection.write('Say: HAHA I win!');
     }
   }
 
